71f372f9212fd4145d2584845ec6baa73576496d
PhongNLTSE171898 <phongnltse171898@fpt.edu.vn>
2024-06-12 14:26:07 +0700

    add feature crud timeSlot

--- src/main/java/com/swp391/teamfour/forbadsystem/model/Yard.java
+++ src/main/java/com/swp391/teamfour/forbadsystem/model/Yard.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,6 +24,11 @@ public class Yard {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "court_id", nullable = false)
     private Court court;
+
+    @ManyToMany
+    @JoinTable(name = "yard_schedule", joinColumns = @JoinColumn(name = "yard_id"),
+            inverseJoinColumns = @JoinColumn(name = "slot_id"))
+    private Collection<TimeSlot> timeSlots;
 
     public Yard(String yardName) {
         this.yardName = yardName;
